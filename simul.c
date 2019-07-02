f33de39d48d2736f534e112963eb16b772b9ad59
peiyi guan <peiyi.guan.yi@gmail.com>
2019-07-02 09:26:40 -0400

    cmdline_args parsed

--- simul.c
+++ simul.c
@@ -12,10 +12,19 @@
 
 #include "args.h"
 #include "error.h"
+/* added lib */
+#include <string.h>
+#include <stdlib.h>
 
-int N=5, T=100;
-double lam=0.1, mu=0.2;
-int nblocked;			/* The number of threads blocked */
+/* default values for args */
+
+int N = 5, T = 100;
+double lam = 0.1, mu = 0.2;
+int nblocked; /* The number of threads blocked */
+
+/*utility functions */
+
+int args_cmdline(int argc, char *argv[]);
 
 
 /***********************************************************************
@@ -23,18 +32,17 @@ int nblocked;			/* The number of threads blocked */
 ************************************************************************/
 void *philosopher(void *vptr)
 {
-  unsigned int seed;		/* This is called from main without    */
-  int pthrerr;			/* creating a new thread               */
+  unsigned int seed; /* This is called from main without    */
+  int pthrerr;       /* creating a new thread               */
   struct thread_arg *ptr;
 
-  ptr = (struct thread_arg*)vptr;
+  ptr = (struct thread_arg *)vptr;
 
   while (1)
-    {
-      fatalerr("philosopher", 0, "Not yet implemented");
-    }
+  {
+    fatalerr("philosopher", 0, "Not yet implemented");
+  }
   return NULL;
-      
 }
 
 /***********************************************************************
@@ -46,28 +54,95 @@ void *clk(void *vptr)
   int pthrerr;
   struct thread_arg *ptr;
 
-  ptr = (struct thread_arg*)vptr;
+  ptr = (struct thread_arg *)vptr;
 
   fatalerr("server", 0, "Not yet implemented");
   exit(0);
 }
+
+
 
 /***********************************************************************
                                M A I N
 ************************************************************************/
 int main(int argc, char **argv)
 {
-  int pthrerr, i;
-  int nserver, nclient, nticks;
-  float lam, mu;
 
-  pthread_t whatever;
-  pthread_cond_t something;
-  pthread_mutex_t blah;
+
+  args_cmdline(argc,argv);
+
+  int pthrerr, i;
+  int nserver, nclient, nticks = T, nphil = N;
+  // double lam = lam
+  // , mu = mu;
+/*
+    pthread_t tid; 
+    int x = pthread_create (&tid, blah, blah, blah);
+
+ */
+
+
+  pthread_t tid;
+  pthread_cond_t cv,chair,start_line; // all phil thread block on start_line condition var.
+
+  pthread_mutex_t mutex;
+
+
+  /*
+  struct thread_arg
+{
+  int N, T;
+  float lam, mu;
+  pthread_cond_t *cv;
+  pthread_mutex_t *blah;
+  unsigned int seed;		Needed for the random number genarator 
+};
+*/
+  
   struct thread_arg *allargs;
+  allargs->N = N;
+  allargs->lam = lam;
+  allargs->mu = mu;
+  allargs->cv = cv;
+  allargs->mutex = PTHREAD_MUTEX_INITIALIZER;
+  allargs->seed = 1;
+
+
+
+
+
   pthread_t *alltids;
 
+  
 
-  printf("Should not be here\n");
+  // printf("Should not be here\n");
+  // printf("lamuda is %f \n", lam);
+  //  printf("mu is %f \n", mu);
+  // printf("T is %d \n", nticks);
+  // printf("N is %d \n", nphil);
   exit(-1);
 }
+
+int args_cmdline(int argc, char *argv[])
+{
+  for (int i = 0; i < argc; i++)
+  {
+    if (strcmp(argv[i], "-L") == 0)
+    {
+      lam = atof(argv[i + 1]);
+    }
+    if (strcmp(argv[i], "-M") == 0)
+    {
+      mu = atof(argv[i + 1]);
+    }
+    if (strcmp(argv[i], "-T") == 0)
+    {
+      T = atof(argv[i + 1]);
+    }
+    if (strcmp(argv[i], "-N") == 0)
+    {
+      N = atof(argv[i + 1]);
+    }
+  }
+}
+
